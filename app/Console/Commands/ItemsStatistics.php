c07ad64894ca08cd8bdb797089a0b4933c09edbf
Atom <albaraa456@gmail.com>
2023-06-12 17:33:16 +0300

    items statistics cli command

---
+++ app/Console/Commands/ItemsStatistics.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Item;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
+class ItemsStatistics extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'items:stats {--a|average} {--H|highest} {--m|current-month} {--t|total}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Display Statistics for the wishlist items';
+
+    protected $statistics = [
+        'average',
+        'highest',
+        'current-month',
+        'total',
+    ];
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $options = collect($this->options())
+            ->filter(fn ($value, $option) => in_array($option, $this->statistics))
+            ->filter(fn ($value, $option) => $value)
+            ->keys();
+
+        $data = $options->isEmpty() ?
+            $this->getAll()
+            : $options->map(fn ($option) => $this->getSelected($option))
+            ->toArray();
+
+        $this->table(['Key', 'Value'], $data);
+    }
+
+    public function getSelected(String $options)
+    {
+        return $this->{'get' . Str::studly($options)}();
+    }
+
+    private function getAll()
+    {
+        return [
+            $this->getTotal(),
+            $this->getAverage(),
+            $this->getHighest(),
+            $this->getCurrentMonth(),
+        ];
+    }
+
+    private function getTotal()
+    {
+        return [
+            'Key' => 'Total',
+            'Value' => Item::count()
+        ];
+    }
+
+    private function getAverage()
+    {
+        return [
+            'Key' => 'Average Price',
+            'Value' => Item::average('price')
+        ];
+    }
+
+    private function getHighest()
+    {
+        return [
+            'Key' => 'Highest Website Prices',
+            'Value' => Item::all()
+                ->groupBy(fn ($item) => parse_url($item->url, PHP_URL_HOST))
+                ->map(fn ($items) => $items->sum('price'))
+                ->sortDesc()
+                ->keys()
+                ->first()
+        ];
+    }
+
+    private function getCurrentMonth()
+    {
+        return [
+            'Key' => 'Current Month Prices',
+            'Value' => Item::currentMonth()->sum('price')
+        ];
+    }
+}
